e5a014b111f58844469c7d38facb0ceab85e9fb7
Hook25 <massigi97@gmail.com>
2022-07-19 23:16:01 +0200

    Removed useless calls and error handling, improved Color

--- spine3/slots.py
+++ spine3/slots.py
@@ -1,86 +1,55 @@
+from .utils import Color
+
 class SlotData:
-    def __init__(self, name, bone_data):
-        if not name:
-            raise ValueError('Name cannot be None.')
-
-        if not bone_data:
-            raise ValueError('bone_data cannot be None.')
-
+    def __init__(self, name, bone_data, color = Color(255, 255, 255, 255), attachment_name = None):
         self.name = name
         self.bone_data = bone_data
-        self.r = 255
-        self.g = 255
-        self.b = 255
-        self.a = 255
-        self.attachment_name = None
+        self.color = color
+        self.attachment_name = attachment_name
     @classmethod
     def build_from(cls, slot_map, skeleton_data):
         slot_name = slot_map['name']
         bone_name = slot_map['bone']
         bone_data = skeleton_data.find_bone(bone_name)
 
-        slot_data = cls(name=slot_name, bone_data=bone_data)
-            
-        if 'color' in slot_map:
-            color_hex = slot_map['color']
-            slot_data.r = int(color_hex[0:2], 16)
-            slot_data.g = int(color_hex[2:4], 16)
-            slot_data.b = int(color_hex[4:6], 16)
-            slot_data.a = int(color_hex[6:8], 16)
+        color_hex = slot_map.get('color', "ff" * 4)
+        color = Color.parse(color_hex)
 
-        if 'attachment' in slot_map:
-            slot_data.attachment_name = slot_map['attachment']
+        attachment_name = slot_map.get('attachment', None)
+        
+        slot_data = cls(
+            name=slot_name, 
+            bone_data=bone_data, 
+            color=color, 
+            attachment_name = attachment_name
+        )
 
         return slot_data
             
 class Slot:
     def __init__(self, slot_data, skeleton, bone):
-        if not slot_data:
-            raise Exception('slotData cannot be None.')
-        if not skeleton:
-            raise Exception('skeleton cannot be None.')
-        if not bone:
-            raise Exception('bone cannot be None.')
         self.data = slot_data
         self.skeleton = skeleton
         self.bone = bone
-        self.r = 255
-        self.g = 255
-        self.b = 255
-        self.a = 255
+        self.color = self.data.color
         self.attachment = None
         self.attachment_time = 0.0
         self.set_to_bind_pose()
-
-
-    def set_color(self, r, g, b, a):
-        self.r = r
-        self.g = g
-        self.b = b
-        self.a = a
                           
-        
     def set_attachment(self, attachment):
         self.attachment = attachment
         self.attachment_time = self.skeleton.time
-
-
-    def set_attachment_time(self, time):
-        self.attachment_time = self.skeleton.time - time
-
-
-    def get_attachment_time(self):
-        return self.skeleton.time - self.attachment_time
-
     
     def set_to_bind_pose(self):
         for i, slot in enumerate(self.skeleton.data.slots):
             if self.data == slot:
                 self.set_to_bind_pose_with_index(i)
 
-
     def set_to_bind_pose_with_index(self, slotIndex):
-        self.set_color(self.data.r, self.data.g, self.data.b, self.data.a)
-        self.set_attachment(self.skeleton.get_attachment_by_index(slotIndex, self.data.attachment_name) if self.data.attachment_name else None)
+        self.color = self.data.color
+        self.set_attachment(
+            self.skeleton.get_attachment_by_index(slotIndex, self.data.attachment_name) 
+                if self.data.attachment_name else None
+        )
     
     
