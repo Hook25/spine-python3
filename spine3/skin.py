9223c72452f3115882c9c8a7749856e297bf0f9e
Hook25 <massigi97@gmail.com>
2022-07-19 23:50:45 +0200

    Recovered missing return

--- spine3/skin.py
+++ spine3/skin.py
@@ -4,30 +4,17 @@ Key = namedtuple("Key", ["slot_index", "name"])
 
 class Skin:
     def __init__(self, name):
-        if not name:
-            raise Exception('Name cannot be None.')
         self.name = name
         self.attachments = {}
 
     def add_attachment(self, slot_index, name, attachment):
-        if not name:
-            raise Exception('Name cannot be None.')        
         key = Key(slot_index=slot_index, name=name)
         self.attachments[key] = attachment
 
     def get_attachment(self, slot_index, name):
         key = Key(slot_index=slot_index, name=name)
-        if key in self.attachments:
-            return self.attachments[key]
-        return None
-            
-    def attach_all(self, skeleton, oldSkin):
-        for key, attachment in self.attachments.items():
-            slot = skeleton.slots[key.slot_index]
-            if skeleton.slots[key.slot_index].attachment == attachment:
-                new_attachment = self.get_attachment(key.slot_index, key.name)
-                if new_attachment:
-                    skeleton.slots[key.slot_index].set_attachment(new_attachment)
+        return self.attachments[key]
+
     @classmethod
     def build_from(cls, slot_map, skin_name, skeleton_data, scale, attachment_loader):
         skin_spec = cls(skin_name)
@@ -39,3 +26,4 @@ class Skin:
                 skin_spec.add_attachment(slot_index, attach_name, attachment)
         if skin_name == 'default':
             skeleton_data.default_skin = skin_spec
+        return skin_spec
